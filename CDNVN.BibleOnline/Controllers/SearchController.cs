2c18bc0f449d6c644dffa59e8bb56efca4432dc1
vietpham <kenboy2304@gmail.com>
2014-08-04 14:08:57 +0700

    update search

--- CDNVN.BibleOnline/Controllers/SearchController.cs
+++ CDNVN.BibleOnline/Controllers/SearchController.cs
@@ -22,34 +22,43 @@ namespace CDNVN.BibleOnline.Controllers
             ViewBag.Bible = new SelectList(db.Bibles, "Version", "Name", v);
             ViewBag.FromBook = new SelectList(db.Books.Where(b => b.Bible.Version.ToLower() == v), "Order", "Name", from);
             ViewBag.ToBook = new SelectList(db.Books.Where(b => b.Bible.Version.ToLower() == v), "Order", "Name", to);
-
-            if (string.IsNullOrWhiteSpace(q)) return View();
-            ViewBag.Title =q;
-            var key = RemoveVietnamese.Change(q);
-            var contents =
-                db.Contents.Where(c => c.Book.Bible.Version.ToLower() == v.ToLower())
-                .Where(c => c.Book.Order >= from && c.Book.Order <= to)
-                .Where(c => c.Tag.Contains(key))
-                .OrderBy(c => c.BookId).ThenBy(c => c.Chapter).ThenBy(c => c.Verse).ToPagedList(page, 20);
-
-
-            return View(contents);
+            return View();
         }
 
-        public ActionResult Json(string v = "", string q = "",int from =1, int to=66, int page = 1)
+        public ActionResult Json(string v = "", string q = "", int from = 1, int to = 66, int page = 1)
         {
+            if (string.IsNullOrWhiteSpace(v) || string.IsNullOrWhiteSpace(q))
+            {
+                return null;
+            }
             q = HttpUtility.HtmlDecode(q);
             if (string.IsNullOrWhiteSpace(v))
                 v = "btt";
             if (string.IsNullOrWhiteSpace(q)) return Json(null, JsonRequestBehavior.AllowGet);
-            var key = RemoveVietnamese.Change(q);
+            var key = RemoveVietnamese.Change(q).ToLower();
             var contents =
                 db.Contents.Where(c => c.Book.Bible.Version.ToLower() == v.ToLower())
-                .Where(c => c.Book.Order >= from && c.Book.Order <= to)
-                .Where(c => c.Tag.Contains(key))
-                .OrderBy(c => c.BookId).ThenBy(c => c.Chapter).ThenBy(c => c.Verse).ToPagedList(page, 20);
+                    .Where(c => c.Book.Order >= from && c.Book.Order <= to)
+                    .Where(c => c.Tag.ToLower().Contains(key))
+                    .OrderBy(c => c.BookId).ThenBy(c => c.Chapter).ThenBy(c => c.Verse).ToPagedList(page, 15);
 
-            return Json(contents.Select(c => new { Url = Url.Action("Index", "Read", new { v = v, q = c.Book.Name + " " + c.Chapter + ":" + c.Verse }), Adress = c.Book.Name + " " + c.Chapter + ":" + c.Verse, Word = c.Word }), JsonRequestBehavior.AllowGet);
+            return Json(new
+            {
+                end = true,
+                total = contents.TotalItemCount,
+                contents = contents.Select(c => new
+                {
+                    Url = Url.Action("Index", "Read", new {v = v, q = c.Book.Name + " " + c.Chapter + ":" + c.Verse}),
+                    BookName = c.Book.Name,
+                    Adress = c.Chapter + ":" + c.Verse,
+                    Word =
+                        c.Word.Replace(c.Word.Substring(c.Tag.ToLower().IndexOf(key.ToLower()), q.Length),
+                            "<strong style='color:red;'>" +
+                            c.Word.Substring(c.Tag.ToLower().IndexOf(key.ToLower()), q.Length) + "</strong>")
+                }),
+            },
+
+        JsonRequestBehavior.AllowGet);
         }
 
 
